167a23948146d5a89bc926abc6097b97c18dfce2
luong <luongdev15@gmail.com>
2020-08-31 21:07:03 +0700

    Init

---
+++ precache-manifest.bef03fd0e3409a77ed028aac135eac48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d2a4adaf019491b09710054d91c9da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e028707de90f4c865bb8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e028707de90f4c865bb8",
+    "url": "/static/js/main.34089ef1.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
